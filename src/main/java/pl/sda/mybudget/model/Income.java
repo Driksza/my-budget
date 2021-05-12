85fe6f695a8609188a2f03da1e67eb1498f2fca9
drikszajd <d_rikszajd@o2.pl>
2021-05-12 20:01:28 +0200

    [add] - first rest controller

--- src/main/java/pl/sda/mybudget/model/Income.java
+++ src/main/java/pl/sda/mybudget/model/Income.java
@@ -1,4 +1,19 @@
 package pl.sda.mybudget.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import pl.sda.mybudget.model.enumeration.IncomeType;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+
 public class Income {
+        long amountInPolishGrosz;
+        private LocalDate incomeDate;
+        private String incomeSource;
+        private IncomeType incomeType;
+
+
 }
---
+++ src/main/java/pl/sda/mybudget/model/enumeration/IncomeType.java
@@ -0,0 +1,8 @@
+package pl.sda.mybudget.model.enumeration;
+
+public enum IncomeType {
+    JOB,
+    ADDITIONAL,
+    MOPS
+
+}
