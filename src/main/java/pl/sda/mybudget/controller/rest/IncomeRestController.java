cfe32f84009bcac265dd13a10ae63deebe3e8e69
drikszajd <d_rikszajd@o2.pl>
2021-05-19 18:52:24 +0200

    [Rest] - Income

--- src/main/java/pl/sda/mybudget/controller/rest/IncomeRestController.java
+++ src/main/java/pl/sda/mybudget/controller/rest/IncomeRestController.java
@@ -1,10 +1,12 @@
 package pl.sda.mybudget.controller.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sda.mybudget.model.Income;
 import pl.sda.mybudget.model.enumeration.IncomeType;
+import pl.sda.mybudget.service.IncomeService;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,13 +15,26 @@ import java.util.List;
 @RequestMapping("/rest/incomes")
 public class IncomeRestController {
 
-    @GetMapping
-    List<Income> getAllIncomes() {
-        return List.of(
-                new Income(1_000, LocalDate.now(), "Od żony:)", IncomeType.MOPS),
-                new Income(5_00_000, LocalDate.now(), "500++", IncomeType.MOPS)
-        );
+
+    private final IncomeService incomeService;
+
+    public IncomeRestController(IncomeService incomeService) {
+        this.incomeService = incomeService;
     }
 
+    @GetMapping
+    List<Income> getAllIncomes() {
+//        return List.of(
+//                new Income(1_000, LocalDate.now(), "Od żony:)", IncomeType.MOPS),
+//                new Income(5_00_000, LocalDate.now(), "500++", IncomeType.MOPS)
+//                );
+        return  incomeService.findAllIncomes();
+    }
+    // Select by id
+    // rest/incomes/1
+    @GetMapping("/{id}")
+    Income findbyId (@PathVariable("id") Long idik) {
+        return null;
+    }
 
 }
